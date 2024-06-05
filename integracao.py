deaf8826add070e3c507f77ba372f4ad47344984
viniciusrabello <viniciusrabello@matematica.ufrj.br>
2024-06-05 15:42:09 -0300

    logger e  continuação da implementacao do programa para integrar as bases de dados

--- integracao.py
+++ integracao.py
@@ -1,21 +1,26 @@
 import pandas as pd
+import time
 
 # função para printar entrada do recibo
 
+colunas_recibo = ['NomeRecibos', 'MunicipioRecibo',
+                  'DistritoRecibo', 'Ano', 'IdRecibo']
+colunas_censo = ['NomeCenso', 'MunicipioCenso',
+                 'DistritoCenso', 'Ano', 'Semelhanca', 'IdCenso']
+
+# horario que começou a ser feita as mudanças para ser o nome do arquivo de log
+log_date = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+
 
 def printar_recibo(entrada_recibo):
-    colunas = ['NomeRecibos', 'MunicipioRecibo',
-               'DistritoRecibo', 'Ano', 'IdRecibo']
-    for coluna, valor in zip(colunas, entrada_recibo):
+    for coluna, valor in zip(colunas_recibo, entrada_recibo):
         print(f'{coluna}: {valor}')
 
 # função para printar candidatos
 
 
 def printar_candidato(entrada_candidato):
-    colunas = ['NomeCenso', 'MunicipioCenso',
-               'DistritoCenso', 'Ano', 'Semelhanca', 'IdCenso']
-    for coluna, valor in zip(colunas, entrada_candidato):
+    for coluna, valor in zip(colunas_censo, entrada_candidato):
         print(f'{coluna}: {valor}')
 
 
@@ -31,55 +36,82 @@ i = 0
 # iteraremos sobre cada possível match
 while i < n_entradas:
     match = possiveis_matches.loc[i]  # pegando match
-    entrada_recibo = match[[
-        # definindo colunas relacionadas ao recibo
-        'NomeRecibos', 'MunicipioRecibo', 'DistritoRecibo', 'Ano', 'IdRecibo']]
+    entrada_recibo = match[colunas_recibo]
     # quantos elementos até próximo MatchId = 0
     # número de candidatos
     n_candidatos = list(possiveis_matches['MatchId'].loc[i+1:]).index(0) + 1
-
-    # display
-    print('='*148)
-    print(f'\nExistem {n_candidatos} possíveis matches para {
-          entrada_recibo['NomeRecibos']}:')
-
-    print('\nRecibo: \n')
-    printar_recibo(entrada_recibo)
-    print('\nCandidatos:')
-
-    # iterando sobre os candidatos possíveis para entrada de recibo
     candidatos = possiveis_matches.loc[i: i + n_candidatos - 1]
 
-    # display
-    for j in range(n_candidatos):
-        candidato = candidatos.loc[i + j][[
-            'NomeCenso', 'MunicipioCenso', 'DistritoCenso', 'Ano', 'Semelhanca', 'IdCenso']]
-        print(f'\nCandidato {j + 1}: \n')
-        printar_candidato(candidato)
+    print('='*148)
+    print(f'\nFaltam {n_entradas - i} de {n_entradas} entradas na planilha.\n')
 
+    def mostrar_candidatos():
+        # display
+        print('='*148)
+        print(f'\nExistem {n_candidatos} possíveis matches para {
+            entrada_recibo['NomeRecibos']}:')
+
+        print('\nRecibo: \n')
+        printar_recibo(entrada_recibo)
+        print('\nCandidatos:')
+
+        # iterando sobre os candidatos possíveis para entrada de recibo
+        # display
+        for j in range(n_candidatos):
+            candidato = candidatos.loc[i + j][colunas_censo]
+            print(f'\nCandidato {j + 1}: \n')
+            printar_candidato(candidato)
+
+    mostrar_candidatos()
     # selecionando candidato ideal
-    while True:
+    escolhido = False
+    while not escolhido:
+        print('='*148)
         print('\nDigite o número do candidato escolhido: (0 para não escolher nenhum)\n')
         escolha = input('')
-        print('\n')
+        print('')
 
         # checando se é int
         try:
             escolha = int(escolha)
         except ValueError:
-            print('Valor inserido é inválido!')
+            print('Valor inserido é inválido! Escolha novamente.')
             continue
         # se 0 for escolhido continue para próximo nome
         if escolha == 0:
             i += n_candidatos
-            break
+            escolhido = True
         else:
+            # checar se número escolhido é válido
             try:
-                print(f'O candidato {escolha} foi escolhido!')
-                candidato = candidatos.loc[i + escolha - 1]
+                # confirmar escolha
+                candidato = candidatos.loc[i + escolha - 1][colunas_censo]
+
+                print('='*148)
+                print('Confirma essa mudança?')
+                print('='*148 + '\n\nRecibo: \n')
+                printar_recibo(entrada_recibo)
+                print('='*148 + '\n\nCenso: \n')
                 printar_candidato(candidato)
-                break
-            except ValueError:
-                print('Valor inserido é inválido!')
+                print('='*148)
+                confirmacao = input(
+                    "\nDigite 'S' para sim ou 'N' para não: ").lower()
+                print('')
+                if confirmacao == 's':
+                    print('\n' + '='*148)
+                    print('\nEscolha confirmada!\n')
+
+                    # logar mudança
+
+                    with open(f'logs/{log_date}.txt', 'a') as f:
+                        f.write(f"{entrada_recibo['NomeRecibos']} | {entrada_recibo['IdRecibo']} ---> IdCenso: {candidato['IdCenso']} (Candidato {escolha})\n")
+
+                    i += n_candidatos
+                    escolhido = True
+                else:
+                    mostrar_candidatos()
+                    continue
+            except:
+                print('Valor inserido é inválido! Escolha novamente.')
                 continue
     continue
